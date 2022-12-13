4c70dddd8a625af3544de125849c996d1371a3ba
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-13 10:20:10 +0530

    leave

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { FormsModule } from '@angular/forms';
-
+import { Routes } from '@angular/router';
+import { MainNavComponent } from './main-nav/main-nav.component';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminLoginComponent
+  },
+  // {
+  //   path:"ul",
+  //   component:EmpLoginComponent
+  // },
+  // {
+  //   path:"ur",
+  //   component:EmpRegisComponent
+  // },
+  // {
+  //   path:"ts",
+  //   component:AddTaskComponent
+  // }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
